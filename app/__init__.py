22cc083b9f2a21e4869e131e6df052ab9ceaa651
Dave-mash <macharia3041@gmail.com>
2019-01-17 21:58:57 +0300

    [finishes #163269715] Configure PostgreSQL into the project

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,18 @@
 from flask import Flask
-from .api.v2.views.user_views import v2 as user_v2    
-from .api.v2.views.meetup_views import v2 as meetup_v2    
-from .api.v2.views.question_views import v2 as question_v2    
-from config import app_config
+from app.db_config import create_tables, conn
+# from .api.v2.views.user_views import v2 as user_v2    
+# from .api.v2.views.meetup_views import v2 as meetup_v2    
+# from .api.v2.views.question_views import v2 as question_v2    
+from instance.config import app_config
 
 def create_app(config_name="development"):
     app = Flask(__name__)
-    app.url_map.strict_slashes = False
-    app.config.from_object(app_config["development"])
-    app.register_blueprint(user_v2)
-    app.register_blueprint(meetup_v2)
-    app.register_blueprint(question_v2)
+    # app.url_map.strict_slashes = False
+    # app.config.from_object(app_config["development"])
+    # app.register_blueprint(user_v2)
+    # app.register_blueprint(meetup_v2)
+    # app.register_blueprint(question_v2)
+    create_tables()
+    cur = conn.cursor()
 
     return app
